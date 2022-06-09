dec50079e6ac0fc52ad0e431a48d52d78f40dcb8
Aleksandra Popova <alexnika@bk.ru>
2022-06-09 18:33:19 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//1.Найти сумму цифр числа.
+
+int n = 123;
+int Sum(int n)
+{
+ if(n < 1) return 0;
+ else return Sum (n / 10) + n % 10;
+}
+Console.WriteLine(Sum(n));
+
+//////////////////////////////////
+
+uint SumDigits(uint n) => n<10 ? n : n%10 + SumDigits(n/10);
+Console.Write("Enter your number: ");
+uint n = uint.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Sum of digits {SumDigits(n)}");
